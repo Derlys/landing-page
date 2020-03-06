3b818c4024660390e12b038b9d4a08db40e309c2
Derlys <derlys275@gmail.com>
2020-03-05 19:39:45 -0500

    baner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { StoriesComponent } from './stories/stories.component';
 const routes:Routes = [
   {
-    path: "", component: HomeComponent,
+    path: "home", component: HomeComponent,
 },
 {
   path: "stories", component: StoriesComponent,
